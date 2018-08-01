232f48b3aa3ca4ccdccbd6bce5dc92e8fb16187d
praisenarsie <xxx420blazeitivanxxx@gmail.com>
2018-08-01 15:31:03 +0100

    Fixes error in frontend.py

--- tools/mapmerge2/Prepare Maps.bat
+++ tools/mapmerge2/Prepare Maps.bat
@@ -4,7 +4,7 @@ cd ../../maps/
 for /R %%f in (*.dmm) do copy "%%f" "%%f.backup"
 
 cls
-echo All dmm files in _maps directories have been backed up
+echo All dmm files in maps directories have been backed up
 echo Now you can make your changes...
 echo ---
 echo Remember to run mapmerge.bat just before you commit your changes!
